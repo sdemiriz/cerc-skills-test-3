ab8bfc006d4ca3cd99a9ff2de6d1f8d98954a562
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-26 20:36:15 -0800

    feat: add population-based coloring to reference PC plots

--- scripts/generate_pc_plots.py
+++ scripts/generate_pc_plots.py
@@ -4,10 +4,25 @@ import pandas as pd
 reference_pc = pd.read_csv(snakemake.input["reference_pc"], sep="\t", header=None)
 reference_pc.columns = ["SAMPLE", "PC1", "PC2", "PC3", "PC4"]
 
+samples_populations = pd.read_csv(
+    snakemake.input["samples_populations"], sep="\t", header=None
+)
+samples_populations.columns = ["SAMPLE", "POPULATION"]
+
+reference_pc_populations = reference_pc.merge(samples_populations, on="SAMPLE")
+
 
 def two_axis_pc_plot(first: str, second: str, out_file: str):
     fig, ax = plt.subplots()
-    ax.scatter(reference_pc[first], reference_pc[second])
+    ax.scatter(
+        x=reference_pc_populations[first],
+        y=reference_pc_populations[second],
+        c=pd.factorize(reference_pc_populations["POPULATION"])[0],
+        alpha=0.3,
+        linewidths=0,
+    )
+    plt.xlabel(first)
+    plt.ylabel(second)
     plt.savefig(out_file)
 
 
