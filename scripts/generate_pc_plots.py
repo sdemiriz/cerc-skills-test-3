fcdc48bf14e81bcbbe3e4a957024b9b9f08a7a3d
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-28 20:27:06 -0800

    feat: add legend in requested format

--- scripts/generate_pc_plots.py
+++ scripts/generate_pc_plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import pandas as pd
 
 reference_pc = pd.read_csv(snakemake.input["reference_pc"], sep="\t", header=None)
@@ -12,27 +13,40 @@ reference_populations.columns = ["SAMPLE", "POPULATION"]
 reference_pc_populations = reference_pc.merge(reference_populations, on="SAMPLE")
 study_pc_populations = pd.read_csv(snakemake.input["ancestry"], sep="\t")
 
+color_map = {
+    "EUR": "red",
+    "SAS": "blue",
+    "EAS": "green",
+    "AFR": "magenta",
+    "AMR": "turquoise",
+}
+
 
 def two_axis_pc_plot(first: str, second: str, out_file: str):
     fig, ax = plt.subplots()
     ax.scatter(
         x=reference_pc_populations[first],
         y=reference_pc_populations[second],
-        c=pd.factorize(reference_pc_populations["POPULATION"])[0],
+        c=reference_pc_populations["POPULATION"].map(color_map),
         alpha=0.3,
         linewidths=0,
     )
     ax.scatter(
         x=study_pc_populations[first],
         y=study_pc_populations[second],
-        marker="^",
         c="black",
+        marker="^",
     )
     for i, sample_number in enumerate(study_pc_populations["SAMPLE"]):
         ax.annotate(
             sample_number,
             (study_pc_populations[first][i], study_pc_populations[second][i]),
         )
+    custom = [
+        Line2D([], [], marker="o", color=clr, linestyle="None")
+        for clr in color_map.values()
+    ] + [Line2D([], [], marker="^", color="black", linestyle="None")]
+    plt.legend(handles=custom, labels=[*color_map.keys(), "Study"])
     plt.xlabel(first)
     plt.ylabel(second)
     plt.savefig(out_file)
